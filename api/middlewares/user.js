095980fcd2db5c1451714ee7ba7db36641f89e84
Henrique Lopes <riquellopes@gmail.com>
2016-09-15 15:01:46 -0300

    criando api basica

--- api/middlewares/user.js
+++ api/middlewares/user.js
@@ -1,5 +1,9 @@
 // User Middleware
 var jwt = require('jwt-simple');
+var log4js = require('log4js');
+
+// Get logger
+var logger = log4js.getLogger("booklog");
 
 // Model
 var User = require("../models/user");
@@ -13,27 +17,34 @@ exports.UserMiddleware = function(request, response, next){
                 (request.headers['x-access-token']);
 
     if(token){
+        logger.info("Token get - ", token);
 
         try{
             var decode = jwt.decode(token, TOKEN_SECRET);
 
             if(decode.exp <= Date.now()){
+                logger.warn("Token expired", decode);
                 response.json(400, {message: "Token expired."});
             }
 
+            //  "-password -__v -create_at"
             User.findOne({_id: decode.iss}, function(error, user){
                 if(error){
+                    logger.warn("Uses does not exist.", error);
                     response.status(500);
                 }
 
+                logger.info("Middleware set user on request ", user);
                 request.user = user;
                 return next();
             });
 
         }catch(error){
+            logger.error("User middleware error - ", error);
             return response.json(401, {message: "Token invalid."});
         }
     } else {
+        logger.warn("Token not found - ", token);
         response.json(401, {message: "Toke not found."});
     }
 }
--- api/views/bookmark.js
+++ api/views/bookmark.js
@@ -2,36 +2,95 @@
 var router = require('express').Router();
 var log4js = require('log4js');
 
+// Get logger
+var logger = log4js.getLogger("booklog");
+
 // Get middleware
 var user = require("../middlewares/user");
 
 // Model
 var Bookmark = require("../models/bookmark");
 
+router.use(user.UserMiddleware);
+
+
+var hide_fields = "-user -__v";
+
 // List all
 router.get("/", function(request, response){
+    var user = request.user;
+
+    Bookmark.find({user:user}, hide_fields, function(error, list){
+        if( error ){
+            logger.error("Error");
+        }
+
+        logger.info("Bookmark size :: user", list.length, user._id);
+        response.json(list);
+    });
 
 });
 
 // Get specific bookmark
 router.get("/:id_bookmark", function(request, response){
+    var user = request.user;
+    var id_bookmark = request.params.id_bookmark;
 
+    logger.info("Get bookmark", id_bookmark, user._id);
+
+    Bookmark.findOne({_id:id_bookmark, user:user}, hide_fields, function(error, document){
+        if(error){
+            logger.error("Error");
+            return response.status(404).json({message: "not found"});
+        }
+
+        response.json(document);
+    });
 });
 
 // Delete specific bookmark
 router.delete("/:id_bookmark", function(request, response){
+    var user = request.user;
+    var id_bookmark = request.params.id_bookmark;
 
+    Bookmark.remove({_id:id_bookmark, user:user}, function(error, document){
+        if(error){
+            logger.error("Error");
+            return response.json({message: "Error."});
+        }
+
+        response.json({message: "Bookmark "+id_bookmark+" removed."});
+    });
 });
 
 // Save bookmark
 router.post("/", function(request, response){
+    var bookmark = new Bookmark();
+        bookmark.user = request.user;
+        bookmark.url = request.body.url;
 
+        bookmark.save(function(error, document){
+            if( error ){
+                return response.status(500).json({message: "Error ..."});
+            }
+
+            response.status(201).json({message: "successfull created."});
+        });
 });
 
 // Update bookmark
-router.put("/:id_bookmark", function(){
+router.put("/:id_bookmark", function(request, response){
+    var user = request.user;
+    var id_bookmark = request.params.id_bookmark;
 
+    Bookmark.findOneAndUpdate({_id:id_bookmark, user:user}, {url:url}, function(error, document){
+        if( error ){
+            logger.error("Error put");
+            return response.status(500).json({message: "Error ..."});
+        }
+
+        response.status(201).json({message: "successfully updated."})
+    });
 });
 
-router.use(user.UserMiddleware);
 module.exports = router;
