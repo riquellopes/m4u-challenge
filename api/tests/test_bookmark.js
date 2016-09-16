aa24388adf409d3bfcdc22bcf9e8c6111a59c66d
Henrique Lopes <riquellopes@gmail.com>
2016-09-16 19:26:17 -0300

    iniciando teste bookmark'

---
+++ api/tests/test_bookmark.js
@@ -0,0 +1,12 @@
+var request = require("supertest");
+var assert = require("assert");
+var app = require("../server").app;
+var log4js = require('log4js');
+var Bookmark = require("../models/bookmark");
+
+describe("create bookmark", function(){
+
+    it("", function(){
+
+    });
+});
