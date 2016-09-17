cd84f50fd39988b918a7600b129d79fd3890f40a
Henrique Lopes <riquellopes@gmail.com>
2016-09-17 17:21:53 -0300

    alterando configuração de produção

--- web/bookmarks/settings/production.py
+++ web/bookmarks/settings/production.py
@@ -5,3 +5,15 @@ DEBUG = False
 ALLOWED_HOSTS = ('*', )
 
 STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(BASE_DIR, "staticfiles"))
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DATABASE_NAME', 'bookmarks'),
+        "USER": os.environ.get("DATABASE_USERNAME", "root"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST", "localhost"),
+        "PORT": os.environ.get("DATABASE_PORT", 3306)
+    }
+}
