7eabdfbb38ddf274661f0b38a6b2781cb7cd7e73
Henrique Lopes <riquellopes@gmail.com>
2016-09-17 22:29:14 -0300

    removendo conf

--- web/bookmarks/settings/production.py
+++ web/bookmarks/settings/production.py
@@ -5,15 +5,3 @@ DEBUG = False
 ALLOWED_HOSTS = ('*', )
 
 STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(BASE_DIR, "staticfiles"))
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'bookmarks'),
-        "USER": os.environ.get("DB_USER", "root"),
-        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
-        "HOST": os.environ.get("DB_PASS", "localhost"),
-        "PORT": os.environ.get("DB_HOST", 3306)
-    }
-}
